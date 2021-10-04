d8531cb778436c3aa6809d78cd34b117029ea09f
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-04 19:06:45 -0300

    Definido layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 class Layout extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0
+        };
+
+        this.addToCounter = this.addToCounter.bind(this);
+    }
+
+    addToCounter() {
+        var count = this.state.count;
+        count++;
+        this.setState({ count: count });
+    }
+
     render() {
         return (
-            <p>Hola, mundo!</p>
+            <div>
+                <button className="btn btn-success" onClick={this.addToCounter}>
+                    <i className="fa fa-plus fa-fw"></i>
+                </button>
+                <hr />
+                <p>Counter: {this.state.count}</p>
+            </div>
         );
     }
 }
